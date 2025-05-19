71cfe30683b45b33f1a77b9086b4e3fb2c25a08b
hongui <honge7694@naver.com>
2025-05-19 20:22:37 +0900

    init commit

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,10 @@
+package io.hhplus.tdd.point;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+}
