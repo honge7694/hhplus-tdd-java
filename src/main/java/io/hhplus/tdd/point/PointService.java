f575ca48e83a0340ce4c8cdf2e4f5150df81a9a9
hongui <honge7694@naver.com>
2025-05-20 23:59:18 +0900

    회원의 포인트를 충전, 히스토리 분리 - 포인트 충전과 히스토리를 분리

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -12,9 +12,17 @@ public class PointService {
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
 
+    /**
+     * User의 포인트를 충전한다.
+     * @param id
+     * @param point
+     * @return
+     */
     public UserPoint chargePoint(Long id, Long point) {
         UserPoint userPoint = userPointTable.selectById(id);
         pointHistoryTable.insert(userPoint.id(), point, TransactionType.CHARGE, System.currentTimeMillis());
         return userPointTable.insertOrUpdate(userPoint.id(), point);
     }
+
+
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -24,28 +24,41 @@ public class PointServiceTest {
     private PointHistoryTable pointHistoryTable;
 
     @Test
-    @DisplayName("회원의 포인트를 충전한다.")
+    @DisplayName("[포인트 충전] - 포인트를 충전한다.")
     public void chargeUserPoint() throws Exception {
         // given
         Long userId = 1L;
         Long chargePoint = 100L;
-        long fixedTime = System.currentTimeMillis();
         UserPoint user = UserPoint.empty(userId);
 
         when(userPointTable.selectById(userId)).thenReturn(user);
-        when(pointHistoryTable.insert(userId, chargePoint, TransactionType.CHARGE, fixedTime))
-                .thenReturn(new PointHistory(1, userId, chargePoint, TransactionType.CHARGE, fixedTime));
         when(userPointTable.insertOrUpdate(user.id(), chargePoint))
                 .thenReturn(new UserPoint(userId, chargePoint, System.currentTimeMillis()));
 
         // when
-        PointHistory userPointHistory = pointHistoryTable.insert(userId, chargePoint, TransactionType.CHARGE, fixedTime);
         UserPoint userPoint = pointService.chargePoint(userId, chargePoint);
 
         // then
         Assertions.assertThat(userPoint.point()).isEqualTo(100L);
-        Assertions.assertThat(userPointHistory.amount()).isEqualTo(100L);
         verify(userPointTable).insertOrUpdate(userId, chargePoint);
+    }
+
+    @Test
+    @DisplayName("[포인트 충전] - 포인트 히스토리를 저장한다.")
+    public void chargeUserPointHistory() {
+        // given
+        Long userId = 1L;
+        Long chargePoint = 100L;
+        long fixedTime = System.currentTimeMillis();
+
+        when(pointHistoryTable.insert(userId, chargePoint, TransactionType.CHARGE, fixedTime))
+                .thenReturn(new PointHistory(1, userId, chargePoint, TransactionType.CHARGE, fixedTime));
+
+        // when
+        PointHistory userPointHistory = pointHistoryTable.insert(userId, chargePoint, TransactionType.CHARGE, fixedTime);
+
+        // then
+        Assertions.assertThat(userPointHistory.amount()).isEqualTo(100L);
         verify(pointHistoryTable).insert(userId, chargePoint, TransactionType.CHARGE, fixedTime);
     }
 }
